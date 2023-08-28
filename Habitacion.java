d4a83b1ac388d578c13f467f7091da167d2aa75f
Nadissa Vela <122498954+nadissa1508@users.noreply.github.com>
2023-08-28 10:25:59 -0600

    Javadoc
    Javadoc de clase Habitacion

--- Habitacion.java
+++ Habitacion.java
@@ -15,6 +15,14 @@ public class Habitacion {
         disponibilidad = true;
     }
 
+    /** 
+     * @param idHabitacion
+     * @param idHuesped
+     * @param tipoHabitacion
+     * @param capOcupantes
+     * @param precioNoche
+     * @param Disponibilidad
+     */
     public Habitacion(int idHabitacion, int idHuesped, int tipoHabitacion, int capOcupantes, float precioNoche, boolean disponibilidad){
         this.idHabitacion = idHabitacion;
         this.idHuesped = idHuesped;
@@ -24,54 +32,105 @@ public class Habitacion {
         this.disponibilidad = disponibilidad;
     }
 
+    
+    /** 
+     * @return int
+     */
     public int getIdHabitacion() {
         return idHabitacion;
     }
 
+    
+    /** 
+     * @param idHabitacion
+     */
     public void setIdHabitacion(int idHabitacion) {
         this.idHabitacion = idHabitacion;
     }
 
+    
+    /** 
+     * @return int
+     */
     public int getIdHuesped() {
         return idHuesped;
     }
 
+    
+    /** 
+     * @param idHuesped
+     */
     public void setIdHuesped(int idHuesped) {
         this.idHuesped = idHuesped;
     }
 
+    
+    /** 
+     * @return int
+     */
     public int getTipoHabitacion() {
         return tipoHabitacion;
     }
 
+    
+    /** 
+     * @param tipoHabitacion
+     */
     public void setTipoHabitacion(int tipoHabitacion) {
         this.tipoHabitacion = tipoHabitacion;
     }
 
+    
+    /** 
+     * @return int
+     */
     public int getCapOcupantes() {
         return capOcupantes;
     }
 
+    
+    /** 
+     * @param capOcupantes
+     */
     public void setCapOcupantes(int capOcupantes) {
         this.capOcupantes = capOcupantes;
     }
 
+    
+    /** 
+     * @return float
+     */
     public float getPrecioNoche() {
         return precioNoche;
     }
 
+    
+    /** 
+     * @param precioNoche
+     */
     public void setPrecioNoche(float precioNoche) {
         this.precioNoche = precioNoche;
     }
 
+     
+     /** 
+      * @return boolean
+      */
      public boolean getDisponibilidad() {
         return disponibilidad;
     }
 
+    
+    /** 
+     * @param disponibilidad
+     */
     public void setDisponibilidad(boolean disponibilidad) {
         this.disponibilidad = disponibilidad;
     }
 
+    /** 
+     * @return String
+     */
     public toString(){
         return "";
     }
